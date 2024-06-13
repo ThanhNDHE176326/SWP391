0851e7308bed1fad1173dd4045e10c4194cf36c8
hainthe172574 <134379702+hainthe172574@users.noreply.github.com>
2024-06-13 23:17:26 +0700

    Hai:  Update view cart.jsp

--- src/java/Controller/cart/ViewCartDetailController.java
+++ src/java/Controller/cart/ViewCartDetailController.java
@@ -2,14 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controller.cart;
 
 import DAO.CartDAO;
 import DAO.CartProductDAO;
 import DAO.CustomerDAO;
 import DAO.ProductDAO;
+import DAO.ProductDAOByPublic;
 import Models.CartProduct;
+import Models.Category;
 import Models.Product;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -26,36 +27,39 @@ import java.util.List;
  *
  * @author admin
  */
-@WebServlet(name="ViewCartDetailController", urlPatterns={"/viewCartDetail"})
+@WebServlet(name = "ViewCartDetailController", urlPatterns = {"/viewCartDetail"})
 public class ViewCartDetailController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ViewCartDetailController</title>");  
+            out.println("<title>Servlet ViewCartDetailController</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ViewCartDetailController at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet ViewCartDetailController at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -63,25 +67,30 @@ public class ViewCartDetailController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         HttpSession session = request.getSession();
         CustomerDAO customerDAO = new CustomerDAO();
         ProductDAO productDAO = new ProductDAO();
         CartDAO cartDAO = new CartDAO();
         CartProductDAO cartProductDAO = new CartProductDAO();
-        
+        ProductDAOByPublic dao = new ProductDAOByPublic();
+
         //lay customerName tren session
         String customerName = (String) session.getAttribute("username");
         int customerID = Integer.parseInt(customerDAO.getInformationCustomer(customerName).getId());
         int cartID = cartDAO.getCartIdByCustomerID(customerID);
-        
+        List<Product> listNewProduct = dao.getTop6ProductNew();
         List<Product> listProductFromCart = cartProductDAO.getProductsFromCartByCartID(cartID);
+        List<Category> categories = dao.getCategory();
+        request.setAttribute("categories", categories);
+        request.setAttribute("listNewProduct", listNewProduct);
         request.setAttribute("listProduct", listProductFromCart);
         request.getRequestDispatcher("view/customer/cart.jsp").forward(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -89,12 +98,13 @@ public class ViewCartDetailController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
