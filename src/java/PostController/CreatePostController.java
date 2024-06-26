c4614cd5dd7190c774b21c67bcae2e6f7bc9b5c0
admin <admin@DESKTOP-QOFOP8B>
2024-06-27 04:01:57 +0700

    Thanh - commit create image blog (no css)

--- src/java/PostController/CreatePostController.java
+++ src/java/PostController/CreatePostController.java
@@ -4,21 +4,34 @@
  */
 package PostController;
 
+import DAO.CategoryBlogDAO;
 import DAO.postDAO;
 import Models.Blog;
+import Models.CategoryBlog;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  *
  * @author Acer
  */
 @WebServlet(name = "CreatePostController", urlPatterns = {"/CreatePost"})
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 2, // 2 MB
+        maxFileSize = 1024 * 1024 * 10, // 10 MB
+        maxRequestSize = 1024 * 1024 * 50 // 50 MB
+)
 public class CreatePostController extends HttpServlet {
 
     /**
@@ -59,6 +72,9 @@ public class CreatePostController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        CategoryBlogDAO categoryBlogDAO = new CategoryBlogDAO();
+        List<CategoryBlog> listCategoryBlog = categoryBlogDAO.getAllCategories();
+        request.setAttribute("listCategoryBlog", listCategoryBlog);
         request.getRequestDispatcher("view/marketing/createpost.jsp").forward(request, response);
     }
 
@@ -74,24 +90,32 @@ public class CreatePostController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         postDAO postDAO = new postDAO();
-
-        String staffId = request.getParameter("staff");
+        
+//        String staffId = request.getParameter("staff");
         String title = request.getParameter("title");
-        String categoryBlogId = request.getParameter("categoryBlogId"); // Fix parameter name
-        String image = request.getParameter("image");
-        String updatedate = request.getParameter("updateDate");
+        String categoryBlogId = request.getParameter("category"); // Fix parameter name
+//        String image = request.getParameter("image");
+        String updatedate = String.valueOf(LocalDate.now());
         String description = request.getParameter("description");
         String content = request.getParameter("content");
-        String status = request.getParameter("status");
+//        String status = request.getParameter("status");
 //        String isDelete = request.getParameter("isDelete"); // Get isDelete parameter
-
-        Blog newPost = new Blog("",staffId, title, categoryBlogId, image, updatedate, description, content, status, "1"); // Pass isDelete to Blog constructor
+        Part part = request.getPart("imageFile");
+        String fileName ="";
+         if (part != null && part.getSize() > 0) {
+            String realPath = "D:\\Web_Project_Java\\SWP391\\web\\images";
+            Files.createDirectories(Paths.get(realPath)); // Tạo thư mục nếu chưa tồn tại
+            fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+            part.write(Paths.get(realPath, fileName).toString());
+            System.out.println("File uploaded to: " + Paths.get(realPath, fileName).toString());
+        }
+        Blog newPost = new Blog("", "4", title, categoryBlogId, fileName, updatedate, description, content, "1", "1"); // Pass isDelete to Blog constructor
         postDAO.addPost(newPost);
 
         response.sendRedirect("postlist");
 
-
     }
+
     /**
      * Returns a short description of the servlet.
      *
